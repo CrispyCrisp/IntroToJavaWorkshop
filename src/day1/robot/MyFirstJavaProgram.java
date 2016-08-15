c95da6deae06ae9f36450ec8390e339eda309eff
CrispyCrisp <league@Mac-mini-2.local>
2016-08-15 15:01:30 -0700

    First Day java Camp
    Made Shapes

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,13 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+Robot thorvo = new Robot ();
+thorvo.penDown();
+thorvo.setPenColor(68,238,68);
+for (int i = 0; i < 4; i++) {
+thorvo.move (300);
+thorvo.turn(-90);
+}
 
-	
-		
-		
 	}
 }
